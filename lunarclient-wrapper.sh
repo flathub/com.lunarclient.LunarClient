25cd8fc56b152274396f81a42805af3a3b11aa68
Donty10 <mtthwdnt@duck.com>
2025-05-14 13:22:01 +0100

    add discord rpc support

---
+++ lunarclient-wrapper.sh
@@ -0,0 +1,9 @@
+# Flatpak Discord RPC support
+
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
+# Run lunarclient
+
+exec /app/bin/lunarclient-wrapped "$@"
